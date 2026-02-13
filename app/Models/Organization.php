fa8dd2430e42ed8b64192eaef1d7811f0c4ca298
anoz05 <159677775+anojpradhan@users.noreply.github.com>
2026-02-13 20:40:17 +0545

    organization relationshipss comments

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -17,22 +17,30 @@ class Organization extends Model
         'created_by',
     ];
 
+    // creator user 
     public function creator()
     {
         return $this->belongsTo(User::class, 'created_by');
     }
+    // all the users in orgs 
     public function users()
     {
         return $this->hasMany(User::class);
     }
+
+    // all the suppliers 
     public function suppliers()
     {
         return $this->hasMany(Supplier::class);
     }
+
+    // all the customers 
     public function customers()
     {
         return $this->hasMany(Customer::class);
     }
+
+    // all purchases in 
     public function purchases()
     {
         return $this->hasMany(Purchase::class);
